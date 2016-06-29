4279d46900f2fc5ea81f1ddd9881f39364aa2e34
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-06-29 14:43:27 +0530

    exercise 4 - study drill 3

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+# Number of available cars
 cars = 100
-space_in_a_car = 4.0
+# Number of available seats in each car
+space_in_a_car = 4
+# Number of available car drivers
 drivers = 30
+# Number of passengers
 passengers = 90
+# Cars that can't be driven = number of cars - number of drivers
 cars_not_driven = cars - drivers
+# Cars that can be driven = number of drivers
 cars_driven = drivers
+# Total number of passengers that can be transported
 carpool_capacity = cars_driven * space_in_a_car
+# Average number of passengers in each car
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
