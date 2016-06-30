a80e5a4901343c5f746eedd8dde985bdf26dcfdc
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-06-30 14:03:05 +0530

    exercise 16 - study drill 3

--- ex16.py
+++ ex16.py
@@ -22,12 +22,7 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + '\n' + line2 + '\n' + line3 + '\n')
 
 print "And finally, we close it."
 target.close()
