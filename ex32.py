ab90d48223bbc684d4e38b569fe8a47c870bdc34
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-07-01 15:34:12 +0530

    exercise 32 - study drill 2

--- ex32.py
+++ ex32.py
@@ -16,13 +16,9 @@ for i in change:
     print "I got %r" % i
 
 # we can also build lists, first start with an empty one
-elements = []
+# assigning a range directly
+elements = range(0, 6)
 
-# then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print "Adding %d to the list." % i
-    # append is a function that lists understand
-    elements.append(i)
 
 # now we can print them out too
 for i in elements:
